679849a38633a7e8b7b483a1fbde405ff2ace70a
JonathanRodriguesJS <202503403961@alunos.estacio.br>
2025-03-28 22:25:05 +0000

    adiciona recursividade para movimentos das peças

--- Xadrez.c
+++ Xadrez.c
@@ -1,38 +1,56 @@
 #include <stdio.h>
 
+void moverTorre(int num){
+    if(num > 0){
+        printf("Direita\n");
+        moverTorre(num - 1);
+    }
+}
+void moverBispo(int num){
+    if(num > 0){
+        for(int i = 0; i < 1; i++){
+            printf("Cima\n");
+            for(int j = 0; j < 1; j++){
+                printf("Direita\n");
+            }
+        }
+        moverBispo(num - 1);
+    }
+}
+void moverRainha(int num){
+    if(num > 0){
+        printf("Esquerda\n");
+        moverRainha(num - 1);
+    }
+}
+void moverCavalo(int num){
+    if(num > 0){
+        if(num == 1){
+            printf("Esquerda\n");
+        }else{
+            printf("Baixo\n");
+            moverCavalo(num - 1);
+        }
+        
+    }
+}
+
 int main() {
     // Movimentação da Torre: 5 casas para a direita
     printf("Movimento da Torre:\n");
-    for (int i = 0; i < 5; i++) {
-        printf("Direita\n");
-    }
+    moverTorre(5);
 
     // Movimentação do Bispo: 5 casas na diagonal para cima e à direita
     printf("\nMovimento do Bispo:\n");
-    int i = 0;
-    while (i < 5) {
-        printf("Cima Direita\n");
-        i++;
-    }
+    moverBispo(5);
 
     // Movimentação da Rainha: 8 casas para a esquerda
     printf("\nMovimento da Rainha:\n");
-    int j = 0;
-    do {
-        printf("Esquerda\n");
-        j++;
-    } while (j < 8);
+    moverRainha(8);
 
     // Movimentação do Cavalo: 2 casa para baixo e uma para esquerda
-    printf("\nMovimento do Cavalo\n");
-    int k,l = 0;
-    for(k = 0; k < 1; k++){
-        while(l < 2){
-            printf("Baixo\n");
-            l++;
-        }
-        printf("Esquerda");
-    }
+    printf("\nMovimento do Cavalo:\n");
+    moverCavalo(3);
 
     return 0;
 }
